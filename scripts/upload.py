8051e6193d30b40377fa430307aa168325fe7f1f
APerson <danielhglus@gmail.com>
2015-12-06 17:47:29 -0500

    [upload.py] Improve "page saved" message

--- scripts/upload.py
+++ scripts/upload.py
@@ -88,6 +88,6 @@ with open("delsort.js", "r") as delsort:
     print("Uploading delsort...")
     result = target.edit(text=new_text, summary=edit_summary)
     if result["edit"]["result"] == "Success":
-        print(colored.green("Done!") + " Uploaded delsort.")
+        print(colored.green("Done!") + " Uploaded delsort to " + root)
     else:
         print(colored.red("Error uploading delsort: ") + result)
